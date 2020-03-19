bdd5cf9ccf2a9ad9b502ef1c37521c4d0fd91b8c
sdemiriz <sedatdemiriz97@gmail.com>
2020-03-19 00:45:09 -0700

    Added: first dotfile iterations

---
+++ polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Terminate running bar instance
+# killall -q polybar
+
+# Launch bar1 and bar2
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# polybar bar1 >>/tmp/polybar1.log 2>&1 &
+# polybar bar2 >>/tmp/polybar2.log 2>&1 &
+
+echo "Bars launched."
